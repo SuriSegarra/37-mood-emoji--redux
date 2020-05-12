8f2fbbd9973a2a9ca9a258952df0baea5dbf3482
surelis segarra <sdarianne34@gmail.com>
2020-05-12 14:26:04 -0700

    created rest of files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Moods from '../containers/Moods';
+import Moods from '../../Containers/Mood';
 
 
 export default function App() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+  
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App/App';
+import App from './components/App';
+import './reset.css';
 
 render(
   <App />,
   document.getElementById('root')
 );
+
