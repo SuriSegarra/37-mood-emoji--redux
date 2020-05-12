d8693481ae42fbee43d29fd7bc62893da4812de4
surelis segarra <sdarianne34@gmail.com>
2020-05-12 15:23:51 -0700

    habdles eat snack action test passed:

--- src/Reducers/MoodReducer.test.js
+++ src/Reducers/MoodReducer.test.js
@@ -19,4 +19,22 @@ describe('drink coffee reducer', () => {
             studies: 0
         });
     });
+
+    it('handles eat snack action',() => {
+        const state = {
+            coffees: 1,
+            snacks: 0,
+            naps: 0,
+            studies: 0
+        }
+        const action = eatSnack();
+        const newState = reducer(state, action);
+
+        expect(newState).toEqual({
+            coffees: 1,
+            snacks: 1,
+            naps: 0,
+            studies: 0
+        });
+    });
 });
