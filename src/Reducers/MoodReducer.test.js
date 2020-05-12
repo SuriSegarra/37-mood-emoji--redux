b6ad1927b5f8b0cc50b88dc3a067e6d9b9efc516
surelis segarra <sdarianne34@gmail.com>
2020-05-12 15:25:55 -0700

    handles take a nap test passed:

--- src/Reducers/MoodReducer.test.js
+++ src/Reducers/MoodReducer.test.js
@@ -37,4 +37,22 @@ describe('drink coffee reducer', () => {
             studies: 0
         });
     });
+
+    it('handles take a nap',() => {
+        const state = {
+            coffees: 1,
+            snacks: 1,
+            naps: 0,
+            studies: 0
+        }
+        const action = takeNap();
+        const newState = reducer(state, action);
+
+        expect(newState).toEqual({
+            coffees: 1,
+            snacks: 1,
+            naps: 1,
+            studies: 0
+        })
+    })
 });
