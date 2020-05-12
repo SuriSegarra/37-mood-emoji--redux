058867df266046a2b214d451eaa8825288f030e2
surelis segarra <sdarianne34@gmail.com>
2020-05-12 16:51:25 -0700

    true if coffee and naps are less than 1 test passed

---
+++ src/Selectors/MoodSelector.test.js
@@ -0,0 +1,12 @@
+import { getCoffees, getFace, getNaps, getSnack, getStudy } from './MoodSelector';
+
+describe('mood selector testing', () => {
+    it('true if coffee and naps are less than 1', () => {
+        const state = {
+            coffees: 0,
+            snacks: 2,
+            naps: 0,
+            studies: 5
+        };
+    });
+});
